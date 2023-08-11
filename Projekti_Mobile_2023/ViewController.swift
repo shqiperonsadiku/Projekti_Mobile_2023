035990509812744e4820fc248bd55e85cf9e1640
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2023-08-11 16:19:44 +0200

    Initial commit

---
+++ Projekti_Mobile_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projekti_Mobile_2023
+//
+//  Created by Shqiperon on 11.8.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
