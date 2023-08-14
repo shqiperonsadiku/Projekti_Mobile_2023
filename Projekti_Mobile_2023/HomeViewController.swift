39bd0215118ee0216a0f865b0816d4bb66da9ad5
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2023-08-14 20:11:21 +0200

    Create HomeViewController.swift

---
+++ Projekti_Mobile_2023/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  ToDoList
+//
+//  Created by Shqiperon Sadiku on 9.8.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        // Do any additional setup after loading the view.
+    }
+}
