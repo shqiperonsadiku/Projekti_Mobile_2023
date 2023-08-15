d5cc8e03ec4c728603b5a8015890de2f6602a492
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2023-08-15 19:11:15 +0200

    Add animation to button and label items

--- Projekti_Mobile_2023/HomeViewController.swift
+++ Projekti_Mobile_2023/HomeViewController.swift
@@ -17,4 +17,29 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
     }
+
+    private func animateLabelFromTop() {
+        titleLabel.transform = CGAffineTransform(translationX: 0, y: -view.bounds.height)
+        UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseInOut, animations: {
+            self.titleLabel.transform = .identity
+        }, completion: nil)
+    }
+    
+    private func animateButtonFromBottom() {
+        startButton.transform = CGAffineTransform(translationX: 0, y: view.bounds.height)
+        UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseInOut, animations: {
+            self.startButton.transform = .identity
+        }, completion: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Animate the appearance of the titleLabel and startButton with a fade-in animation
+        UIView.animate(withDuration: 0.5) {
+            self.titleLabel.alpha = 1
+            self.startButton.alpha = 1
+        }
+    }
+    
 }
