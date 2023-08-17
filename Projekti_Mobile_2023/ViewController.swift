eba2c269e9487f5bf82daa1cc1261ec550ac9a7d
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2023-08-17 18:34:26 +0200

    Add table view and CRUD functions in ViewController

--- Projekti_Mobile_2023/ViewController.swift
+++ Projekti_Mobile_2023/ViewController.swift
@@ -9,11 +9,116 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+
+    private var models = [ToDoListItem]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         title = "To Do List"
+        
+        view.addSubview(tableView)
+        getAllItems()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
 
+    @objc private func didTapAdd() {
+        
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Task name"
+            textField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self] _ in
+            if let field = alert.textFields?.first, let text = field.text, !text.isEmpty {
+                self?.createItem(name: text)
+            }
+        }
+        submitAction.isEnabled = false
+        alert.addAction(cancelAction)
+        alert.addAction(submitAction)
+        
+        present(alert, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
+        
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        cell.contentView.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 20),
+            containerView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -20),
+            containerView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 30),
+            containerView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -30),
+        ])
+        
+        return cell
+    }
+
+    // MARK: Core Data functions
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+            getAllItems()
+            showAlert(with: "Task created successfully")
+        }
+        catch {
+            
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        do {
+            try context.save()
+            getAllItems()
+            showAlert(with: "Task deleted successfully")
+        }
+        catch {
+            
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        do {
+            try context.save()
+            getAllItems()
+            showAlert(with: "Item updated successfully")
+        }
+        catch {
+            // Handle error
+        }
+    }
 
 }
 
